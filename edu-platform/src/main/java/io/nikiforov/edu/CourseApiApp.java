949fbb203a674481130a455ba3b4e2eb5ef4366f
Nikita <nikiforovpizza@gmail.com>
2018-03-10 21:38:26 +0200

    Connect Log4j2

--- edu-platform/src/main/java/io/nikiforov/edu/CourseApiApp.java
+++ edu-platform/src/main/java/io/nikiforov/edu/CourseApiApp.java
@@ -1,5 +1,7 @@
 package io.nikiforov.edu;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,7 +13,7 @@ import org.springframework.web.WebApplicationInitializer;
 @SpringBootApplication
 public class CourseApiApp extends SpringBootServletInitializer
         implements WebApplicationInitializer {
-
+    private static final Logger logger = LogManager.getLogger(CourseApiApp.class);
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
         return application.sources(CourseApiApp.class);
@@ -20,5 +22,10 @@ public class CourseApiApp extends SpringBootServletInitializer
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApp.class, args);
         System.out.println("Hello!");
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Warning!");
+        logger.error("Error log");
+        logger.fatal("Fatall error");
     }
 }
